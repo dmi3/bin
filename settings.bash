246042c56f341f100f08afcdc725ea372cc05956
Dmitry <dima@dmi3.net>
2016-04-18 15:27:38 +0300

    Update settings.bash

--- settings.bash
+++ settings.bash
@@ -2,5 +2,5 @@
 # Do not trim long paths in the prompt
 PROMPT_DIRTRIM=0
 
-# Send terminate on Ctrl+K to free Ctrl+C to copy
-stty intr \^q
+# Send terminate on Ctrl+Shift+C to free Ctrl+C to copy
+stty intr \^C

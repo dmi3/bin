9f17d5e3333d3c4a95dc2714cecac7466b94a2c8
Dmitry <dima@dmi3.net>
2016-05-29 13:41:39 +0300

    Update aliases.bash

--- aliases.bash
+++ aliases.bash
@@ -88,4 +88,6 @@ setup-bash() {
    echo "PROMPT_DIRTRIM=0" >> ~/bin/sensible.bash
    # Send terminate on Ctrl+Shift+C to free Ctrl+C for copy
    echo "stty intr \^C" >> ~/bin/sensible.bash
+   # Setup prompt
+   echo "PS1=\"\[\e[0;02m\][\A \w]$ \[\e[0m\]\"" >> ~/bin/sensible.bash
 }

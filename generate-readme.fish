1ebb33161b5d315331dbc2aeb05010d3d491cb38
dmi3 <dima@dmi3.net>
2017-02-14 18:12:46 +0200

    update README

--- generate-readme.fish
+++ generate-readme.fish
@@ -4,6 +4,6 @@ echo "# Useful scripts for Linux users"
 
 for f in (git ls-files "*[^.md|^.txt]]")
     echo -e "\n# [$f](https://github.com/dmi3/bin/blob/master/$f)\n"    
-    grep -h -e "#\s\s" ~/bin/$f | grep -v "Author\|Source" | string sub -s 4 | string replace "Usage" "\Usage"
+    grep -h -e "#\s\s" ~/bin/$f | grep -v "Author\|Source" | string sub -s 4 | string replace "Usage" "\nUsage"
     echo -e "<hr/>"    
 end

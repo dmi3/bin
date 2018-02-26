df7440cc3efc7cf867bd1b3f1bf4aa9ddb8ef732
Dmitry <dima@dmi3.net>
2018-02-26 18:31:52 +0200

    Fix readme generation

--- generate-readme.fish
+++ generate-readme.fish
@@ -12,7 +12,7 @@
     
 
 for f in (git ls-files "*[^.md|^.txt|.gitignore]")
-    echo -e "\n# [$f](https://github.com/"(basename (pwd))"/bin/blob/master/$f)\n"    
+    echo -e "\n# [$f](https://github.com/dmi3/"(basename (pwd))"/blob/master/$f)\n"    
     grep -h -e "#\s\s" $f | grep -v "Author\|Source" | string sub -s 4 | string replace -ar "(?=Usage|Requirements|Instalation|Decription)" "\n"
     echo -e "<hr/>"    
 end

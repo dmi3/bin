5c9ba91a7c8c6e4709b0aa6e47d693247a66d57b
dmi3 <dima@dmi3.net>
2021-11-19 12:10:42 +0200

    Update README

--- generate-readme.fish
+++ generate-readme.fish
@@ -6,12 +6,17 @@
 
 #  Usage
 #  -----
-#       echo -e "#!/bin/sh\necho \# Useful scripts for Linux users > README.md\necho \"See my [Fish config](https://github.com/dmi3/fish) for more CLI awesomness\" >> README.md\ngenerate-readme.fish >> README.md\nshasum -a 256 * | grep -v 'SHASUMS\|config' > SHASUMS" > .git/hooks/pre-commit
+#       echo -e "#!/bin/sh\necho \# Useful scripts for Linux users > README.md\necho \"See my [Fish config](https://github.com/dmi3/fish) for more CLI awesomness\" >> README.md\ngenerate-readme.fish --reverse >> README.md\nshasum -a 256 * | grep -v 'SHASUMS\|config' > SHASUMS" > .git/hooks/pre-commit
 #       chmod +x .git/hooks/pre-commit    
 
-    
 
-for f in (git ls-files "*[^.md|^.txt]" | tac)
+if [ "$argv[1]" = "--reverse" ]
+  set FILTER tac
+else
+  set FILTER cat
+end
+
+for f in (git ls-files "*[^.md|^.txt]" | $FILTER)
     echo -e "\n# [$f](https://github.com/dmi3/"(basename (pwd))"/blob/master/$f)\n"    
     grep -h -e "#\s\s" $f | grep -v "Author\|Source" | string sub -s 4 | string replace -ar "(?=Usage|Requirements|Instalation|Decription|Description)" "\n"
     cat $f | grep -e "^function" | string replace 'function ' '* `' | string replace ' --description' '`'
--- tomatych.py
+++ tomatych.py
@@ -1,5 +1,6 @@
 #  <img src="http://developer.run/pic/tomatych.png"/>
-#  &nbsp;
+#  Description
+#  -----------
 #  * Simple Hackable Pomodoro Timer with optional Slack and Habitica integrations.
 #  * Intended to be hacked and modified to fit your specific vision of how Pomodoro timers should work.
 #  * Moved to [separate repo](https://github.com/dmi3/tomatych)

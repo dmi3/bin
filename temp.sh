d8b99682826515bdf590f8a8d4d34df26faa69b7
dmi3 <dima@dmi3.net>
2023-06-04 16:20:45 +0300

    Add script that Shows CPU, System and GPU temperature

---
+++ temp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#  Decription
+#  ----------
+#  Shows CPU, System and GPU temperature
+
+#  Author: [Dmitry](http://dmi3.net) [Source](https://github.com/dmi3/bin)
+
+#  Requirements
+#  ------------
+#      sudo apt-get install jq lm-sensors nvidia-smi
+
+#  Usage
+#  -----
+#    temp
+
+CPU=$(sensors -j | jq '."coretemp-isa-0000"."Package id 0".temp1_input')
+SYS=$(sensors -j | jq '."acpitz-acpi-0".temp1.temp1_input')
+GPU=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
+
+echo "CPU:$CPU SYS:$SYS GPU:$GPU"
+
+# https://github.com/dmi3/qmk_firmware/blob/master/keyboards/handwired/onekey/kb2040/kbecho.py
+if type "kbecho.py" > /dev/null; then
+  kbecho.py t "CPU:$CPU SYS:$SYS\nGPU:$GPU"
+fi
+
